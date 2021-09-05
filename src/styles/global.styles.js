a1f6189fbc3f6bf6a11f9bb44d8b66d66a5e9eb0
Moddoo <>
2021-09-05 16:09:57 -0500

    projects

--- src/styles/global.styles.js
+++ src/styles/global.styles.js
@@ -9,7 +9,7 @@ export default createGlobalStyle`
     body {
         padding: 0;
         margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        font-family: "Suisse Intl", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
             Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
         -moz-osx-font-smoothing: grayscale;
         -webkit-font-smoothing: antialiased;
